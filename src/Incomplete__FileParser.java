594a02f5a33967d40f74557e7e8c93965eee4a76
kmedhi <kmedhi@purdue.edu>
2014-03-06 18:33:07 -0500

    added ClassFinder

--- src/Incomplete__FileParser.java
+++ src/Incomplete__FileParser.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import org.json.simple.*;
 import java.util.ArrayList;
+import java.util.regex.*;
  
 public class FileParser {
 	public static JSONObject getFileStruct(String fileName) { 
@@ -8,10 +9,7 @@ public class FileParser {
 		JSONObject fileStruct = new JSONObject();
 		JSONArray members = new JSONArray();
 		JSONArray methods = new JSONArray();
-		
-		int a, b, x, s
-		int akasjd ( sdfsdf );
-		
+
 		try {
 			FileReader file = new FileReader(fileName);
 			BufferedReader reader = new BufferedReader(file);
@@ -19,27 +17,21 @@ public class FileParser {
 			while ( (line = reader.readLine()) != null ) {
 				ArrayList<String> evalResult = new ArrayList<String>();
 				evalResult = evaluate(line);
-				switch (evalResult.get(0)) {
-					case "function":
-						methods.add(evalResult.get(1));				
-						break;
-					case "variable": 
-						for (int i=1; i<evalResult.size(); i++)
-							members.add(evalResult.get(i));
-						break;
-					default:	
-						break;				
-				}
+				if (evalResult.get(0) == "function") 
+					methods.add(evalResult.get(1));				
+				if (evalResult.get(0) == "variable") 
+					for (int i=1; i<evalResult.size(); i++)
+						members.add(evalResult.get(i));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			if (file != null) {
+		} /*finally {
+			if (file) {
 				try {
 					file.close();
 				} catch (IOException e) {}
 			}				
-		}
+		}*/
 		
 		
 		fileStruct.put("members", members);
@@ -55,7 +47,7 @@ public class FileParser {
 		Matcher funcMatches = funcDeclaration.matcher(line);
 		if (funcMatches.find()) {
 			evalResult.add("function");
-			evalResult.add((m.group(1)));
+			evalResult.add((funcMatches.group(1)));
 			return evalResult;
 		}	
 		
@@ -65,7 +57,7 @@ public class FileParser {
 		Matcher varMatches = varDeclaration.matcher(line);
 		if (varMatches.find()) {
 			evalResult.add("variable");
-			line.replaceAll("=[^,]+","");
+			line.replaceAll( "=[^,]+" , "");
 			String[] varList = line.split("\\s*,\\s*");
 			for (String i : varList)
 				evalResult.add(i);
@@ -77,6 +69,8 @@ public class FileParser {
 	}
 
 	/* Diagnostic code */
-	
+	public static void main( String args[] ) {
+		System.out.println(getFileStruct("test.java").toString());
+	}
 	
 }
