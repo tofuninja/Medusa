01e7661c09e52c5978eca91db5cb6aeed43b4199
log <log@purdue.edu>
2014-03-31 18:33:27 -0400

    Updated Diagram and Deleted DiagramStructure

--- src/Diagram.java
+++ src/Diagram.java
@@ -1,22 +1,42 @@
 import java.util.*;
 
 class DiagramBlock {
-	private String name;
+	private String className;
+	private ArrayList<String> methodNames;
+	private ArrayList<String> variableNames;
 	private float x;
 	private float y;
 	
-	public DiagramBlock(String name, float x, float y) {
-		this.name = name;
+	public DiagramBlock(String className, ArrayList<String> methodNames, ArrayList<String> variableNames, float x, float y) {
+		this.className = className;
+		this.methodNames = methodNames;
+		this.variableNames = variableNames;
 		this.x = x;
 		this.y = y;
 	} 
 	
-	// Getter and setter for name
-	public String getName() {
-		return name;
+	// Getter and setter for class name
+	public String getClassName() {
+		return className;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public ArrayList<String> getMethodNames() {
+		return methodNames;
+	}
+
+	public void setMethodNames(ArrayList<String> methodNames) {
+		this.methodNames = methodNames;
+	}
+
+	public ArrayList<String> getVariableNames() {
+		return variableNames;
+	}
+
+	public void setVariableNames(ArrayList<String> variableNames) {
+		this.variableNames = variableNames;
 	}
 	
 	// Getter and setter for x
@@ -37,7 +57,7 @@ class DiagramBlock {
 	
 	@Override
 	public String toString() {
-		return name + ": (" + x + ", " + y + ")";
+		return className + ": (" + x + ", " + y + ")";
 	}
 }
 
@@ -45,10 +65,11 @@ class Diagram {
 	public ArrayList<DiagramBlock> JavaBlocks = new ArrayList<DiagramBlock>();
 	public String Folder;
 	
-	public Diagram(ArrayList<String> JavaClasses, String folder) {
+	public Diagram(ArrayList<JavaClass> JavaClasses, String folder) {
 		Folder = folder;
 		for( int i=0; i<JavaClasses.size(); i++ ) {
-			DiagramBlock b = new DiagramBlock(JavaClasses.get(i), 50 + 200*(i%5), 100 + (i/5)*100);
+			JavaClass jc = JavaClasses.get(i);
+			DiagramBlock b = new DiagramBlock(jc.className, jc.methodNames, jc.variableNames, 50 + 200*(i%5), 100 + (i/5)*100);
 			JavaBlocks.add(b);
 		}
 	}
@@ -63,7 +84,7 @@ class Diagram {
 	public String getCoordinateForJavaClass(String name) {
 		for( int i=0; i<JavaBlocks.size(); i++ ) {
 			DiagramBlock b = JavaBlocks.get(i);
-			if( b.equals(name) ) {
+			if( b.getClassName().equals(name) ) {
 				return b.getX() + "," + b.getY();
 			}
 		}
